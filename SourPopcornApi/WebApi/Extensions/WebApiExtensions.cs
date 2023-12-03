ee88fc9029e0910f07891420ea21181d239c838c
Damynas <povdam6@gmail.com>
2023-12-04 00:40:24 +0200

    CORS added

--- SourPopcornApi/WebApi/Extensions/WebApiExtensions.cs
+++ SourPopcornApi/WebApi/Extensions/WebApiExtensions.cs
@@ -7,6 +7,8 @@ namespace WebApi.Extensions;
 
 public static class WebApiExtensions
 {
+    private const string CorsPolicyName = "SourPopcornCorsPolicy";
+
     public static void RegisterServices(this WebApplicationBuilder builder)
     {
         builder.Services
@@ -14,16 +16,33 @@ public static class WebApiExtensions
             .AddInfrastructure(builder.Environment, builder.Configuration)
             .AddPresentation();
 
+        // Add CORS
+        var allowedOrigins = builder.Configuration.GetSection("CORS:AllowedOrigins");
+        if (allowedOrigins.Value is null)
+            throw new ArgumentException("CORS policy configuration section is incorrect.");
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy(name: CorsPolicyName,
+                builder => builder
+                    .WithOrigins(allowedOrigins.Value)
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+        });
+
         builder.Services.AddAuthMiddleware(builder.Configuration);
     }
 
     public static void RegisterServices(this WebApplication app)
     {
+        app.AddErrorHandlingMiddleware();
+
         app.UseHttpsRedirection();
+        app.UseCors(CorsPolicyName);
+
         app.UseAuthentication();
         app.UseAuthorization();
 
-        app.AddErrorHandlingMiddleware();
         app.AddEndpoints();
     }
 
